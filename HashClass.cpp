db63b7fa44dd3059adbbb2e123c75639763446a1
Aathmika02 <62003916+Aathmika02@users.noreply.github.com>
2023-10-11 09:05:19 +0530

    Update HashClass.cpp-codesmells

--- HashClass.cpp
+++ HashClass.cpp
@@ -19,7 +19,7 @@ class hashing
 			string password;
 			
 	public:
-			void set_password(const string& entered_password)
+			void set_password(std::string_view entered_password)
 			{
 				password = entered_password;
 			}
